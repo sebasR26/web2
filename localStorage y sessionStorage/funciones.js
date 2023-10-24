9f9416ff74b796639b25d2af8260fcdcc501c6a0
MileG <valentina18tabordarico@gmail.com>
2023-10-23 20:29:23 -0500

    terminando crud

--- localStorage y sessionStorage/funciones.js
+++ localStorage y sessionStorage/funciones.js
@@ -41,13 +41,32 @@ let precio = document.querySelector(".precio");
 let imagen = document.querySelector(".imagen");
 let observaciones = document.querySelector(".observaciones");
 let botonGuardar = document.querySelector(".btn-pedido");
+let btnActualizar = document.querySelector(".btn-actualizar");
 let tabla = document.querySelector(".table tbody");
+
 
 //agregar evento al boton
 
 botonGuardar.addEventListener("click", function(){
-obtenerDatos();
+let datos = obtenerDatos();
+guardarDatos(datos);
+limpiarDatos();
+mostrarDatos();
 });
+
+//evento para recargar el navegador
+document.addEventListener("DOMContentLoaded" , function(){
+    limpiarDatos();
+    mostrarDatos();
+})
+
+//duncion para limpiar datos
+function limpiarDatos(){
+    let datosTabla = document.querySelectorAll(".table tbody tr")
+    datosTabla.forEach((fila)=>{
+        fila.remove();
+    })
+}
 
 //funcion para obtener los datos
 function obtenerDatos(){
@@ -63,12 +82,14 @@ function obtenerDatos(){
     }
 
     console.log(datosForm);
-    guardarDatos(datosForm); 
+     
     cliente.value = "";
     producto.value = "";
     precio.value = "";
     imagen.value = "";
     observaciones.value = "";
+    //retornar el objeto
+    return datosForm;
 }
 
 };
@@ -104,11 +125,11 @@ function mostrarDatos(){
             <td> ${dato.producto} </td>
             <td> ${dato.cliente} </td>
             <td> ${dato.precio} </td>
-            <td> <imge src= ${dato.imagen} width="50%"></td>
+            <td> <img src= "${dato.imagen}" width="30%"></td>
             <td> ${dato.observaciones} </td>
             <td> 
                 <span  onclick="editarPedido(${i})" class="btn btn-warning"> Editar </span>
-                <span onclick="eliminarPedido(${i})"  class="btn btn-danger"> Eliminar </span>
+                <span onclick="eliminarDato(${i})"  class="btn btn-danger"> Eliminar </span>
             </td>
 
         `;
@@ -119,4 +140,55 @@ function mostrarDatos(){
 
 }   
 
-mostrarDatos();
+//funcion eliminar datos de localStorage
+function eliminarDato(pos){
+    let pedido = [];
+    let datosDeLocal= JSON.parse( localStorage.getItem(claveLocal));
+    if(datosDeLocal!=null){
+        pedido = datosDeLocal;
+    }
+    let confirmar = confirm("deseas ELIMINAR este dato?")
+    if(confirmar){
+        pedido.splice(pos, 1);
+       // console.log(pedido)
+    }
+    localStorage.setItem(claveLocal,JSON.stringify(pedido));
+    alert("pedido eliminado con exito")
+    limpiarDatos();
+    mostrarDatos();
+}
+
+//editar pedido de localStorage
+function editarPedido(pos){
+    let pedido = [];
+    let datosDeLocal= JSON.parse( localStorage.getItem(claveLocal));
+    if(datosDeLocal!=null){
+        pedido = datosDeLocal;
+    }
+    
+    //pasar los datos al formulario para editarlos
+    cliente.value = pedido[pos].cliente
+    producto.value = pedido[pos].producto
+    precio.value = pedido[pos].precio
+    observaciones.value = pedido[pos].observaciones
+    botonGuardar.classList.toggle("d-none");
+    btnActualizar.classList.toggle("d-none");
+    btnActualizar.addEventListener("click", function(){
+        pedido[pos].cliente = cliente.value;
+        pedido[pos].producto = producto.value;
+        pedido[pos].precio = precio.value;
+        pedido[pos].observaciones = observaciones.value;
+        localStorage.setItem(claveLocal, JSON.stringify(pedido));
+        alert("Pedido actualizado con exito");
+        botonGuardar.classList.toggle("d-none");
+        btnActualizar.classList.toggle("d-none");
+        limpiarDatos();
+        mostrarDatos();
+        cliente.value = "";
+    producto.value = "";
+    precio.value = "";
+    imagen.value = "";
+    observaciones.value = "";
+
+    })
+}
